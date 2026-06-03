cf590365f64305e4eee0fa1eb9511bc8e92ee612
switedev <switedev@gmail.com>
2026-06-03 23:08:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import Response
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"status": "API is alive"}
+
+@app.post("/remove-bg")
+async def remove_bg(image: UploadFile = File(...)):
+
+    from rembg import remove  # safe now
+
+    input_image = await image.read()
+    output = remove(input_image)
+
+    return Response(content=output, media_type="image/png")
