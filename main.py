7d2e1c21e1f9deed01066902cb2b4f500edf1dca
switedev <switedev@gmail.com>
2026-06-03 22:54:35 +0300

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import Response
 from rembg import remove
+import io
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return {"status": "BG Remover API is running"}
+
 @app.post("/remove-bg")
 async def remove_bg(image: UploadFile = File(...)):
+
     input_image = await image.read()
-    output = remove(input_image)
-    return Response(content=output, media_type="image/png")
+
+    try:
+        output = remove(input_image)
+        return Response(content=output, media_type="image/png")
+
+    except Exception as e:
+        return {"error": str(e)}
