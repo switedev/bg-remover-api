eb8025f3fe9d970571bd0c7b90d4f3003ec67994
switedev <switedev@gmail.com>
2026-06-03 23:44:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import Response
-from rembg import remove, new_session
-from PIL import Image
-import io
 
 app = FastAPI()
-
-session = new_session("u2netp")
 
 @app.get("/")
 def home():
@@ -15,22 +10,9 @@ def home():
 @app.post("/remove-bg")
 async def remove_bg(image: UploadFile = File(...)):
 
+    from rembg import remove  # safe now
+
     input_image = await image.read()
-
-    # preprocess
-    img = Image.open(io.BytesIO(input_image)).convert("RGBA")
-    buffer = io.BytesIO()
-    img.save(buffer, format="PNG")
-    input_image = buffer.getvalue()
-
-    # better removal
-    output = remove(
-        input_image,
-        session=session,
-        alpha_matting=True,
-        alpha_matting_foreground_threshold=240,
-        alpha_matting_background_threshold=10,
-        alpha_matting_erode_size=10
-    )
+    output = remove(input_image)
 
     return Response(content=output, media_type="image/png")
